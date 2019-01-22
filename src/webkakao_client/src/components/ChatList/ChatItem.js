56358758f571ebcc471409e20478d8609f6c7be3
YoungHo Jo <zeroho.jo@gmail.com>
2019-01-22 10:14:51 +0900

    Add ChaList
    index.js
        Chat List
    
    ChatItem.js
        Chat Item

---
+++ src/webkakao_client/src/components/ChatList/ChatItem.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './styles.scss'
+
+class ChatItem extends React.Component {
+
+  render() {
+    return(
+      <div className="ChatItem">
+        <div className="profileContainer">
+
+        </div>
+
+        <div className="senderMsgContainer">
+
+        </div>
+
+        <div className="timeContainer">
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ChatItem
