c5fd61fea33fd4d3ad6a9a2e711f60dc7ca4aff1
HongSungMoon <hsm63746244@gmail.com>
2019-01-23 10:16:35 +0900

    add :  - API Server initial commut

---
+++ src/APIServer/src/main/java/com/webkakao/api/ApiServerApplication.java
@@ -0,0 +1,14 @@
+package com.webkakao.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerApplication.class, args);
+	}
+
+}
+
