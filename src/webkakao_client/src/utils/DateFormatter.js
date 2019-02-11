4b3d6df34df2eb3302de89e9e4b16b3ce3a0a30c
HongSungMoon <hsm63746244@gmail.com>
2019-02-11 23:53:42 +0900

    fix :  - getKoreanDate Util 오류 수정

--- src/webkakao_client/src/utils/DateFormatter.js
+++ src/webkakao_client/src/utils/DateFormatter.js
@@ -3,7 +3,7 @@ module.exports = {
   getKoreanDate: (timestamp) => {
     const time = new Date(timestamp);
     let hour = time.getHours();
-    const AMPM = hour >= 12 ? '오전' : '오후';
+    const AMPM = hour <= 12 ? '오전' : '오후';
     hour = hour % 12;
     const minute = time.getMinutes();
     const fullTime = AMPM + " " + hour + ":" + (minute <= 10 ? '0' + minute : minute);
