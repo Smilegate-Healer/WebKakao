ad1e0c35866032d7054f030b3bdc60b410668fe6
YoungHo Jo <zeroho.jo@gmail.com>
2019-02-18 23:16:44 +0900

    FriendList 코드 정리

--- src/webkakao_client/src/components/FriendList/FriendItem.js
+++ src/webkakao_client/src/components/FriendList/FriendItem.js
@@ -1,76 +1,68 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Typography } from '@material-ui/core'
-import DefaultProfileImg from '../../resources/img_person_no1.png'
-import { observer, inject } from 'mobx-react';
-import "./styles.scss"
+import React from "react";
+import PropTypes from "prop-types";
+import { Typography } from "@material-ui/core";
+import DefaultProfileImg from "../../resources/img_person_no1.png";
+import { observer, inject } from "mobx-react";
+import "./styles.scss";
 
 @inject("stores")
 @observer
 class FriendItem extends React.Component {
-
   onClick = () => {
     const { user } = this.props.stores;
     const req = {
       user_idx: this.props.user.user_idx
-    }
+    };
     user.getUserInfo(req);
-  }
-  
+  };
 
-  _renderProfile = (profile_img) => {
+  _renderProfile = profile_img => {
     return (
       <img
         className="profile"
         alt="profile"
-        src={profile_img ? `http://localhost:8083/profile/${profile_img}` : DefaultProfileImg} 
+        src={
+          profile_img
+            ? `http://localhost:8083/profile/${profile_img}`
+            : DefaultProfileImg
+        }
       />
-    )
-  }
-
-  _randerFunc = () => {
-    const { user } = this.props
-    if (!user.hide) {
-      return (<div className="Item" onClick={this.onClick}>
-        {/* <div className="profileContainer" onClick={this.props.onProfileClick}> */}
-        <div className="profileContainer">
-          {this._renderProfile(user.profile_img)}
-        </div>
-
-        <div className="nameStatusMsgContainer">
-          <div className="name">
-            <Typography className="text" variant="body1" color="textPrimary">
-              {user.name}
-            </Typography>
-          </div>
-          <div className="statusMsg">
-            <Typography variant="body2" color="textSecondary">
-              {user.status_msg}
-            </Typography>
-          </div>
-        </div>
-      </div>)
-    } else {
-      return (<div></div>)
-    }
-  }
-
+    );
+  };
+  
   render() {
-    const component = this._randerFunc();
+    const { user } = this.props;
+    if (!user.hide) {
+      return (
+        <div className="FriendItem" onClick={this.onClick}>
+          <div className="profileContainer">
+            {this._renderProfile(user.profile_img)}
+          </div>
 
-    return (
-      <div>
-        { component }
-      </div>
-    )
-
+          <div className="nameStatusMsgContainer">
+            <div className="name">
+              <Typography className="text" variant="body1" color="textPrimary">
+                {user.name}
+              </Typography>
+            </div>
+            <div className="statusMsg">
+              <Typography variant="body2" color="textSecondary">
+                {user.status_msg}
+              </Typography>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <div />;
+    }
   }
 
   static defaultProps = {
     user: {
       hide: false
     }
-  }
+  };
 }
 
 FriendItem.propTypes = {
@@ -81,7 +73,7 @@ FriendItem.propTypes = {
     hide: PropTypes.bool
   }),
   onClick: PropTypes.func,
-  onProfileClick: PropTypes.func,
-}
+  onProfileClick: PropTypes.func
+};
 
-export default FriendItem
+export default FriendItem;
--- src/webkakao_client/src/components/PasswordReset/index.js
+++ src/webkakao_client/src/components/PasswordReset/index.js
@@ -1,50 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { observer, inject } from "mobx-react";
 
 @inject("stores")
 @observer
 class PasswordReset extends Component {
+  state = {
+    email: "",
+    name: ""
+  };
 
-    state = {
-        email: '',
-        name: ''
-    }
+  onLogin = () => {
+    debugger;
+    const { user } = this.props.stores;
+    user.resetPassword(this.state);
+  };
 
-    onLogin = () => {
-        debugger;
-        const { user } = this.props.stores;
-        user.resetPassword(this.state);
-    }
+  emailChange = e => {
+    this.setState({
+      email: e.target.value
+    });
+  };
 
-    emailChange = (e) => {
-        this.setState({
-            email: e.target.value,
-        });
-    }
+  nameChange = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
 
-    nameChange = (e) => {
-        this.setState({
-            name: e.target.value
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <form>
-                    <h2> Password Reset </h2>
-                    <br></br>
-                    <label> E-Mail </label>
-                    <input type="email" id="inputEmail" value={this.state.email} placeholder="E-Mail" required onChange={this.emailChange} />
-                    <label> name </label>
-                    <input type="text" id="inputPassword" value={this.state.name} placeholder="Name" required onChange={this.nameChange} />
-                    <br></br>
-                </form>
-                <button type="submit" onClick={this.onLogin}> Password Reset
-                </button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <form>
+          <h2> Password Reset </h2>
+          <br />
+          <label> E-Mail </label>
+          <input
+            type="email"
+            id="inputEmail"
+            value={this.state.email}
+            placeholder="E-Mail"
+            required
+            onChange={this.emailChange}
+          />
+          <label> name </label>
+          <input
+            type="text"
+            id="inputPassword"
+            value={this.state.name}
+            placeholder="Name"
+            required
+            onChange={this.nameChange}
+          />
+          <br />
+        </form>
+        <button type="submit" onClick={this.onLogin}>
+          {" "}
+          Password Reset
+        </button>
+      </div>
+    );
+  }
 }
 
 export default PasswordReset;
