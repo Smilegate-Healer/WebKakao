2b4b120960ddbefa80d805fbb6141d241fb1d529
HongSungMoon <hsm63746244@gmail.com>
2019-02-18 23:44:14 +0900

    fix :  - 대화방에 본인만 남았을 경우 오류 수정

--- src/webkakao_client/src/utils/ChatroomNameFormatter.js
+++ src/webkakao_client/src/utils/ChatroomNameFormatter.js
@@ -1,9 +1,12 @@
 module.exports = {
   getChatroomName: (list, chatroom_name) => {
-    
     if (!chatroom_name) {
 
       let chatroomName = '';
+
+      if(!list) {
+        return '대화상대 없음'
+      }
 
       for (var i = 0; i < list.length - 1; i++) {
         chatroomName += list[i].name + ', ';
