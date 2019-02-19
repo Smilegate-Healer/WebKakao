9fd6b4d4979ad5352cb1b7831205076f5d38dda6
HongSungMoon <hsm63746244@gmail.com>
2019-02-19 20:19:23 +0900

    add :  - Message Type 추가

--- src/ChattingServer/src/main/java/com/healer/webkakao/chatting/util/MessageType.java
+++ src/ChattingServer/src/main/java/com/healer/webkakao/chatting/util/MessageType.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 public enum MessageType {
-  m("message"), p("photo"), v("video"), f("file");
+  m("message"), p("photo"), v("video"), f("file"), i("invite");
 
   final private String type;
 
