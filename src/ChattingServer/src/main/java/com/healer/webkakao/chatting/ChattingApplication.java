69e17df6a07e3bd1a22ebcbe3f63b2958aa47f4f
YoungHo Jo <zeroho.jo@gmail.com>
2019-02-25 16:58:02 +0900

    Add war properties

--- src/ChattingServer/src/main/java/com/healer/webkakao/chatting/ChattingApplication.java
+++ src/ChattingServer/src/main/java/com/healer/webkakao/chatting/ChattingApplication.java
@@ -2,13 +2,19 @@ package com.healer.webkakao.chatting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChattingApplication {
+public class ChattingApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(ChattingApplication.class, args);
   }
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(ChattingApplication.class);
+  }
 }
 
--- src/fileserver/src/main/java/com/healer/webkakao/fileserver/FileserverApplication.java
+++ src/fileserver/src/main/java/com/healer/webkakao/fileserver/FileserverApplication.java
@@ -2,13 +2,19 @@ package com.healer.webkakao.fileserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FileserverApplication {
+public class FileserverApplication extends SpringBootServletInitializer  {
 
   public static void main(String[] args) {
     SpringApplication.run(FileserverApplication.class, args);
   }
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(FileserverApplication.class);
+  }
 }
 
